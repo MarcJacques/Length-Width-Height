efef2d344fdd1bb6a808e42b3c729a863902fc13
Marc Jacques <39604945+MarcJacques@users.noreply.github.com>
2023-01-15 20:56:52 +0000

    filled out algorithm

--- .ccls-cache/@home@runner@LengthWidthHeight/main.cpp
+++ .ccls-cache/@home@runner@LengthWidthHeight/main.cpp
@@ -1,5 +1,24 @@
 #include <iostream>
+using namespace std;
 
-int main() {
-  std::cout << "Hello World!\n";
+int main() 
+{
+
+  int length, width, perimeter, area;
+  //read length and width of rectangle
+  cout << "What is the length of the rectangle? ";
+  cin >> length;
+
+   cout << "What is the width of the rectangle? ";
+    cin >> width; 
+  //compute perimeter
+    perimeter = (length * 2) + (width * 2)
+  //compute area
+    area = length * width;
+  //display all info
+  cout << "length = " << length << endl;
+  cout << "width = " << width << endl;
+  cout << "perimeter of rectangle = " << perimeter << endl;
+  cout << "area of rectangle = " << area << endl;
+  return 0;
 }
